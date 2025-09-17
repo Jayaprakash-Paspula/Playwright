117868676c962b2fd57bbfba59ae0bc92d576ee1
Jayaprakash-Paspula <134996192+Jayaprakash-Paspula@users.noreply.github.com>
2025-09-17 11:27:59 +0530

    Create playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,16 @@
+// playwright.config.js
+module.exports = {
+  testDir: "./tests",
+  retries: 1,
+  reporter: [["html", { outputFolder: "playwright-report" }]],
+  use: {
+    headless: true,
+    screenshot: "only-on-failure",
+    video: "retain-on-failure"
+  },
+  projects: [
+    { name: 'Chromium', use: { browserName: 'chromium' } },
+    { name: 'Firefox', use: { browserName: 'firefox' } },
+    { name: 'WebKit', use: { browserName: 'webkit' } }
+  ]
+};
